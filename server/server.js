d27d23b22cf981c21c67e53f19ca1de5e30d623a
So6oN_Maximix <maxime.leost@gmail.com>
2026-07-25 15:54:58 +0200

    Modified server PORT

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const authRoutes = require("./routes/auth");
 const crypto = require("crypto");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const root = path.join(__dirname, "..");
 app.use(express.json());
